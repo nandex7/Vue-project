5afbde7022119127573766d758a4a586c6718761
Fernando Terrazas <fernando.terrazas.a@gmail.com>
2021-09-08 21:47:02 -0400

    Vue Project with Vite package

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="center">
+  <p >  {{count}} </p>
+  <button @click="addCount">add</button>
+</div>  
+</template>
+
+
+<script>
+  export default{
+    data() {
+      return {
+        count:1
+      };
+    },
+    methods: {
+      addCount() {
+        this.count++;
+
+      }
+          },
+    
+
+  };
+</script>
+
+
+<style>
+.center {
+  height:100vh;
+  display:flex;
+  align-items:center;
+}
+</style>
